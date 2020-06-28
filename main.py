72fe9af093b5758260d846978c19411a58f54bc6
annehs <annehoogerduijn@gmail.com>
2020-06-28 11:10:57 +0200

    main.py ff, cf

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from model_merge import EvacuationModel
 def main(iter, N, h, w, hex, pr, ff, cf):
 
     push_ratio = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-    
+
     for pr in push_ratio:
 
         print("RUNNING Evacution model for " + str(iter) + ' ITERATION(S)\n' +
@@ -24,12 +24,12 @@ def main(iter, N, h, w, hex, pr, ff, cf):
 
         swap_times = np.zeros((iter, 1500))
         for i in range(iter):
-            evacModel = EvacuationModel(N, w, h, hex, pr) 
+            evacModel = EvacuationModel(N, w, h, hex, pr)
             swap_times_current_run = evacModel.run_model()[1]
             for a in swap_times_current_run:
                 swap_times[i][a] += 1
             #swap_times[i] = swap_times_current_run
-            
+
         #print(exit_times)
         # Name of .npy file to save exit times.
         #print(swap_times)
@@ -44,4 +44,4 @@ def main(iter, N, h, w, hex, pr, ff, cf):
 if __name__ == '__main__':
     N = [30,40,50,60,70,80,90,100,110,120,130,140]
     for i in N:
-        main(iter=100, N=i, h=71, w=71, hex=True, pr=0.5)
+        main(iter=100, N=i, h=71, w=71, hex=True, pr=0.5, ff=0, cf=0)
